ee0701d76217527a515c915a427ac455fdcc0df3
onlymash <im@mash.im>
2019-01-14 17:01:36 +0800

    Tags order by date desc

--- moebooru/src/main/kotlin/im/mash/moebooru/common/data/local/dao/TagDao.kt
+++ moebooru/src/main/kotlin/im/mash/moebooru/common/data/local/dao/TagDao.kt
@@ -6,7 +6,7 @@ import io.reactivex.Flowable
 
 @Dao
 interface TagDao {
-    @Query("SELECT * FROM tags WHERE site = :site")
+    @Query("SELECT * FROM tags WHERE site = :site ORDER BY uid DESC")
     fun getTags(site: String): Flowable<MutableList<Tag>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
